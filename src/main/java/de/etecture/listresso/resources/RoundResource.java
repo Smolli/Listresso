1c1bd3000585948e746381a5bb39b693744e818f
Christian Smolka <christian.smolka@etecture.de>
2015-10-04 15:00:44 +0200

    Resourcen-Dummys hinzugefügt

---
+++ src/main/java/de/etecture/listresso/resources/RoundResource.java
@@ -0,0 +1,9 @@
+package de.etecture.listresso.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/round")
+public class RoundResource {
+}
---
+++ src/main/java/de/etecture/listresso/resources/dto/MemberDTO.java
@@ -0,0 +1,4 @@
+package de.etecture.listresso.resources.dto;
+
+public class MemberDTO {
+}
